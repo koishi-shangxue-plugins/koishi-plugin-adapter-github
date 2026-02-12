5c2bbeb1479451610159f857e2b34b19093db762
shangxueink <1919892171@qq.com>
2026-02-13 02:59:03 +0800

    fix(package): update version to 0.3.2
    refactor(src/index.ts): fix indentation and link formatting in usage info
    
    build(tsconfig): update build info with modern target and options

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,7 @@ export const usage = `
 - **Webhook 模式**：实时接收 GitHub 事件推送（需要公网 URL），支持完整接收 GitHub 事件
 - **Pull 模式**：定时轮询获取事件（支持代理 且 无需公网 URL），部分事件不支持，例如 Discussion 事件
 
-详细配置说明请参考 [README 文档](https://koishi-shangxue-plugins.github.io/koishi-plugin-adapter-github/)
-`;
+详细配置说明 请参考 <a href="https://koishi-shangxue-plugins.github.io/koishi-plugin-adapter-github/" target="_blank" class="iirose-link">README 文档</a>`;
 
 // 插件入口
 export function apply(ctx: Context, config: Config)
